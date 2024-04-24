dcd9c14a9caac2ca2cbadce73e2659cd99017c8c
A'zamjon Alijonov <alijonovrobo@gmail.com>
2024-04-24 21:28:57 +0500

    Add project files.

---
+++ Extension/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string name = "A'zamjon";
+        name.Hi();
+    }
+}
+
+static class NewMethodClass
+{
+    public static void Hi(this string e)
+    {
+        Console.WriteLine("Assalomu alaykum");
+    }
+}
